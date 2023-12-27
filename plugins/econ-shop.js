8878497ee4102d83bc6aab133e84eabe0f6b1677
Rian88768 <133885786+Rian88768@users.noreply.github.com>
2023-12-27 16:57:29 +0700

    econ-shop.js

--- plugins/econ-shop.js
+++ plugins/econ-shop.js
@@ -1,17 +1,17 @@
 //import db from '../lib/database.js'
 
-const xpperdiamond = 350 
+const xpperlimit = 350 
 let handler = async (m, { conn, command, args }) => {
   let count = command.replace(/^buy/i, '')
-  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperdiamond) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
+  count = count ? /all/i.test(count) ? Math.floor(global.db.data.users[m.sender].exp / xpperlimit) : parseInt(count) : args[0] ? parseInt(args[0]) : 1
   count = Math.max(1, count)
-  if (global.db.data.users[m.sender].exp >= xpperdiamond * count) {
-    global.db.data.users[m.sender].exp -= xpperdiamond * count
+  if (global.db.data.users[m.sender].exp >= xpperlimit * count) {
+    global.db.data.users[m.sender].exp -= xpperlimit * count
     global.db.data.users[m.sender].diamond += count
     conn.reply(m.chat, `
 ┌─「 *NOTA PEMBAYARAN* 」
 ‣ *Nominal pembelian* : + ${count}💎 
-‣ *Usang* : -${xpperdiamond * count} XP
+‣ *Usang* : -${xpperlimit * count} XP
 └──────────────`, m)
   } else conn.reply(m.chat, `❎ Maaf, Anda tidak memiliki cukup *XP* untuk membeli *${count}* Berlian\n\n Anda bisa mendapatkan *XP* menggunakan perintah💎 di menu *games &; economy*`, m)
 }
